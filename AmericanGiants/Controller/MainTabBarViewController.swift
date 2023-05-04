008c03a24e445448f9ba183e6bbe240966de5d13
EzhilNUIT2123 <ezhilarasi.kumaresan@nulogic.io>
2023-05-04 15:28:01 +0530

    inital commit

---
+++ AmericanGiants/Controller/MainTabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmericanGiants
+//
+//  Created by Ezhilarasi on 04/05/23.
+//
+
+import UIKit
+
+class MainTabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
