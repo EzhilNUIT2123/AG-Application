204b2374bdc25ba96e46292791f06d5d90ffa1b5
EzhilNUIT2123 <ezhilarasi.kumaresan@nulogic.io>
2023-05-08 15:38:27 +0530

    complete tabBar changes

--- AmericanGiants/Controller/MainTabBarViewController.swift
+++ AmericanGiants/Controller/MainTabBarViewController.swift
@@ -11,8 +11,24 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        
         view.backgroundColor = .systemPink
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: CartViewController())
+        let vc3 = UINavigationController(rootViewController: UserViewController())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc1.tabBarItem.title = "Home"
+        vc2.tabBarItem.image = UIImage(systemName: "cart")
+        vc2.tabBarItem.title = "Cart"
+        vc3.tabBarItem.image = UIImage(systemName: "person.circle")
+        vc3.tabBarItem.title = "Profile"
+        
+        tabBar.tintColor = .label
+        
+        setViewControllers([vc1,vc2,vc3], animated: true)
     }
 
 
